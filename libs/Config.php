ccd3dd541c70650a76615e96f67dee9dc3ef1016
Alberto Berroteran <aberroteran@live.com>
2014-10-27 13:04:19 -0430

    Separate method

--- libs/Config.php
+++ libs/Config.php
@@ -25,6 +25,7 @@ class Config{
         'ini'  => '\Ashrey\Config\Parser\Ini',
     );
 
+
     /**
      * Read a config file
      *
@@ -35,19 +36,43 @@ class Config{
     public function read($file, $force = FALSE)
     {
         /*is it memory?*/
+        $file = realpath($file);
         if (isset($this->_conf[$file]) && !$force){
             return isset($this->_conf[$file]);
         }
+        return $this->parser($file);
+    }
 
+    /**
+     * Parse a file
+     * @param  string $file  path of file
+     * @return array
+     */
+    public function parser($file){
         $ext = pathinfo($file, PATHINFO_EXTENSION);
         if(!isset($this->_parser[$ext])){
             throw new \RuntimeException("Extension not register");
         }
         $class = $this->_parser[$ext];
-        $text  = file_get_contents($file);
         $obj   = new $class();
-        $this->_conf[$file] = $obj->parse($text);
-        return $this->_conf[$file];
+        $this->_conf[$file] = $obj->parse($this->readfile($file));
+
+
+
     }
+
+    /**
+     * Read a file
+     * @param  string $file path
+     * @return string
+     */
+    protected function readfile($file){
+        if(!is_readable($file)){
+            throw new \RuntimeException("$file no is readable  file");
+        }
+        return file_get_contents($file);
+    }
+
+
 
 }
